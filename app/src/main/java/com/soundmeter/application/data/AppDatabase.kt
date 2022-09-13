3d7001c98e15f099c3aac0c97d80710778b2182e
nanditya@nusantarabetastudio.com <66107893+NandityaNuswatama@users.noreply.github.com>
2022-09-13 16:28:57 +0700

    Add noise result and average db

--- app/src/main/java/com/soundmeter/application/data/AppDatabase.kt
+++ app/src/main/java/com/soundmeter/application/data/AppDatabase.kt
@@ -1,13 +1,14 @@
 package com.soundmeter.application.data
 
+import androidx.room.AutoMigration
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 import com.soundmeter.application.utils.ListStringConverter
 
-@Database(entities = [SoundEntity::class], version = 1, exportSchema = false)
+@Database(entities = [SoundEntity::class], version = 3, exportSchema = true, autoMigrations = [AutoMigration(from = 1, to = 2)])
 @TypeConverters(ListStringConverter::class)
-abstract class AppDatabase: RoomDatabase() {
-    
+abstract class AppDatabase : RoomDatabase() {
+
     abstract fun soundDao(): SoundDao
 }
